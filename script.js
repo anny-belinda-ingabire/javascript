7e3b469601d7981870cf7c8744dedbdc4c0c936f
Jules257 <julesnzejimana@gmail.com>
2023-08-13 00:17:22 +0200

    changes in function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tbody = document.querySelector('tbody');
-let url ='http://localhost/crudjavascript';
+let url ='http://localhost/crudjavascript/mobiles';
+let mobiles =[];
 
 
 
@@ -10,10 +11,10 @@ function getMobiles(){
 
 
 fetch(url)
-.then(response=>response)
+.then(response=>response.json())
 .then(data=>{
     console.log(data)
-    crudjavascript = data;
+    mobiles = data;
     updateTable();
 
 })
@@ -24,7 +25,22 @@ fetch(url)
 
 getMobiles();
 function updateTable(){
-    
+    let data = "";
+   if(crudjavascript.length>0){
+    for(i=0;i<getMobiles.length;i++){
+data+= `<tr>
+<td>${crudjavascript[i]['name']}</td>
+<td>${crudjavascript[i]['price']}</td>
+<td>${crudjavascript[i]['ram']+"GB"}</td>
+<td>${crudjavascript[i]['storage']}</td>
+
+
+
+</tr>
+`
+    }
+    console.log(data)
+   } 
 }
 
 
